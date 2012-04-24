4fea6bece102b1b73b6de87a0391e2df368aa4d7
Tim Smith <tim.smith@webtrends.com>
2012-04-24 11:28:56 -0700

    Remove the deprecated sun java packages if remove_deprecated_packages attribute is set (defaults to false)

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,6 +17,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# remove the deprecated Ubuntu jdk packages
+default['java']['remove_deprecated_packages'] = false
+
 # default jdk attributes
 default['java']['install_flavor'] = "openjdk"
 default['java']['jdk_version'] = '6'
--- recipes/default.rb
+++ recipes/default.rb
@@ -19,3 +19,12 @@
 #
 
 include_recipe "java::#{node['java']['install_flavor']}"
+
+# Purge the deprecated Sun Java packages if remove_deprecated_packages is true
+%w[sun-java6-jdk sun-java6-bin sun-java6-jre].each
+do |pkg|
+  package pkg do
+    action :purge
+    only_if { node['java']['remove_deprecated_packages'] }
+  end
+end
