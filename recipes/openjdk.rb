2afcb3e06aa9a4f99e06ce54150457037243d204
Pat Collins <collins@maya.com>
2012-05-06 11:23:01 -0400

    Fixes OpenJDK 7 package install on Ubuntu Precise 12.04 on 64-bit platform.

--- recipes/openjdk.rb
+++ recipes/openjdk.rb
@@ -53,6 +53,7 @@ if platform?("ubuntu","debian","redhat","centos","fedora","scientific","amazon")
         # not currently set jdk 7 as the default jvm on installation
         require "fileutils"
         arch = node['kernel']['machine'] =~ /x86_64/ ? "x86_64" : "i386"
+        arch = 'amd64' if arch == 'x86_64' && node["platform_version"].to_f >= 12.04
         Chef::Log.debug("glob is #{java_home_parent}/java*#{version}*openjdk*")
         jdk_home = Dir.glob("#{java_home_parent}/java*#{version}*openjdk{,[-\.]#{arch}}")[0]
         Chef::Log.debug("jdk_home is #{jdk_home}")
