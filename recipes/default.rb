a5c2585b74739df9deaee428573340b0a8470140
jhowarth <jhowarth@riotgames.com>
2012-05-16 13:51:21 -0700

    Fixed syntax error on ruby 1.9.2.

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,8 +21,7 @@
 include_recipe "java::#{node['java']['install_flavor']}"
 
 # Purge the deprecated Sun Java packages if remove_deprecated_packages is true
-%w[sun-java6-jdk sun-java6-bin sun-java6-jre].each
-do |pkg|
+%w[sun-java6-jdk sun-java6-bin sun-java6-jre].each do |pkg|
   package pkg do
     action :purge
     only_if { node['java']['remove_deprecated_packages'] }
