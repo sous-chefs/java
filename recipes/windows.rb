769b07fa0649dec8050d47596eaa7d24cc252ba4
martink <kendrick.martin@webtrends.com>
2012-12-19 08:59:36 -0800

    COOK-2095 Fixing the copyright for tas50's sake

--- recipes/windows.rb
+++ recipes/windows.rb
@@ -3,7 +3,7 @@
 # Cookbook Name:: java
 # Recipe:: windows
 #
-# Copyright 2008-2011, Opscode, Inc.
+# Copyright 2008-2012 Webtrends, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
