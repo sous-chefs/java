30861f2d4368dc5aae0e6489a64f556c9c741d5e
jtimberman <joshua@opscode.com>
2013-05-13 17:57:23 -0600

    [COOK-2904] - add helper for returning java path
    Add a helper in libraries/ to determine the correct location for the
    java installation so `update-alternatives` can detect the right place.
    
    This doesn't use any shell globbing and instead uses the fact that the
    path is "standardized" (see COOK-2904) across Debian and RHEL
    platforms.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'chefspec'
 
 RSpec.configure do |config|
+  config.color_enabled = true
+  config.tty = true
+  config.formatter = :documentation
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
