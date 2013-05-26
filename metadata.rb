8d5b48b23578684c34333662519f90f41a8b4aac
Christophe Arguel <christophe.arguel@free.fr>
2013-05-26 14:35:08 +0200

    [COOK-3029] - Add recipe for managing Oracle Java RPM.
    oracle_rpm recipe assumes the Oracle java RPM is available in a custom
    YUM repositories.
    
    This recipe is configured with the following attributes:
    
          default['java']['install_flavor'] = 'oracle_rpm'
          default['java']['oracle_rpm']['type'] = 'jdk'
    
    The type attribute is either "jdk" or "jre".
    
    This recipe also sets the JAVA_HOME environment variable and enables
    the commands included in the RPM with the update-alternatives.

--- metadata.rb
+++ metadata.rb
@@ -10,6 +10,7 @@ recipe "java", "Installs Java runtime"
 recipe "java::openjdk", "Installs the OpenJDK flavor of Java"
 recipe "java::oracle", "Installs the Oracle flavor of Java"
 recipe "java::oracle_i386", "Installs the 32-bit jvm without setting it as the default"
+recipe "java::oracle_rpm", "Installs the Oracle RPM flavor of Java"
 
 
 %w{ debian ubuntu centos redhat scientific fedora amazon arch oracle freebsd windows suse }.each do |os|
