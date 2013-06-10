343b969d08e1cf6632ccfd82cc64c9ce2e0fa8a4
jtimberman <joshua@opscode.com>
2013-06-10 16:34:12 -0600

    [COOK-3142] - missed a space in another spec

--- spec/windows_spec.rb
+++ spec/windows_spec.rb
@@ -1,7 +1,7 @@
 require 'chefspec'
 
 describe 'java::windows' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'java::windows' }
+  let(:chef_run) { ChefSpec::ChefRunner.new.converge 'java::windows' }
   it 'should do something' do
     pending 'Your recipe examples go here.'
   end
