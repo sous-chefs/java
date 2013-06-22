deed8f0a906183c30dcc7a9d5ce215e800829d61
Eric Saxby <sax@livinginthepast.org>
2013-06-21 23:30:36 -0700

    [COOK-2154] Add SmartOS support to the openjdk recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -25,6 +25,7 @@ default['java']['install_flavor'] = "openjdk"
 default['java']['jdk_version'] = '6'
 default['java']['arch'] = kernel['machine'] =~ /x86_64/ ? "x86_64" : "i586"
 default['java']['openjdk_packages'] = []
+default['java']['accept_license_agreement'] = false
 
 case node['platform_family']
 when "rhel", "fedora"
@@ -43,6 +44,9 @@ when "windows"
 when "debian"
   default['java']['java_home'] = "/usr/lib/jvm/default-java"
   default['java']['openjdk_packages'] = ["openjdk-#{node['java']['jdk_version']}-jdk", "default-jre-headless"]
+when "smartos"
+  default['java']['java_home'] = "/opt/local/java/sun6"
+  default['java']['openjdk_packages'] = ["sun-jdk#{node['java']['jdk_version']}", "sun-jre#{node['java']['jdk_version']}"]
 else
   default['java']['java_home'] = "/usr/lib/jvm/default-java"
   default['java']['openjdk_packages'] = ["openjdk-#{node['java']['jdk_version']}-jdk"]
