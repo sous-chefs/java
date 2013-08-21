9082cab0183aabfdb5c86f9598b98c302445fb17
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-21 18:31:38 -0400

    Created an attribute called ['java']['windows']['options'] to take the place of the hardcoded /s in the java windows recipe. Now users will be able to pass in their own options and not have to create a override cookbook for one simple change

--- attributes/default.rb
+++ attributes/default.rb
@@ -41,6 +41,7 @@ when "windows"
   default['java']['install_flavor'] = "windows"
   default['java']['windows']['url'] = nil
   default['java']['windows']['package_name'] = "Java(TM) SE Development Kit 7 (64-bit)"
+  default['java']['windows']['options'] = '/s'
 when "debian"
   default['java']['java_home'] = "/usr/lib/jvm/default-java"
   default['java']['openjdk_packages'] = ["openjdk-#{node['java']['jdk_version']}-jdk", "default-jre-headless"]
