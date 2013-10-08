2f136505dcae26def2aa187afa218d22d01f3eda
Jarek Gawor <jgawor@gmail.com>
2013-10-08 01:39:59 -0400

    allow file urls

--- resources/ark.rb
+++ resources/ark.rb
@@ -19,7 +19,7 @@
 
 actions :install, :remove
 
-attribute :url, :regex => /^https?:\/\/.*(tar.gz|tgz|bin|zip)$/, :default => nil
+attribute :url, :regex => /^(file|https?):\/\/.*(tar.gz|tgz|bin|zip)$/, :default => nil
 attribute :mirrorlist, :kind_of => Array, :default => nil
 attribute :checksum, :regex => /^[a-zA-Z0-9]{40,64}$/, :default => nil
 attribute :app_home, :kind_of => String, :default => nil
