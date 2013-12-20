3b6422c4307f4c60d3691683c8dda96d53dd336b
David Bernick <dbernick@broadinstitute.org>
2013-12-20 14:04:00 -0500

    [COOK-4136] - adding md5 parameter to java_ark
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- resources/ark.rb
+++ resources/ark.rb
@@ -24,6 +24,7 @@ state_attrs :alternatives_priority,
             :app_home_mode,
             :bin_cmds,
             :checksum,
+            :md5,
             :default,
             :mirrorlist,
             :owner,
@@ -32,6 +33,7 @@ state_attrs :alternatives_priority,
 attribute :url, :regex => /^(file|https?):\/\/.*(tar.gz|tgz|bin|zip)$/, :default => nil
 attribute :mirrorlist, :kind_of => Array, :default => nil
 attribute :checksum, :regex => /^[0-9a-f]{32}$|^[a-zA-Z0-9]{40,64}$/, :default => nil
+attribute :md5, :regex => /^[0-9a-f]{32}$|^[a-zA-Z0-9]{40,64}$/, :default => nil
 attribute :app_home, :kind_of => String, :default => nil
 attribute :app_home_mode, :kind_of => Integer, :default => 0755
 attribute :bin_cmds, :kind_of => Array, :default => nil
