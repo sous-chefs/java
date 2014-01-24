bd5e78abd0c5040ce78215ca88a512ff4312f48b
Chris Armstrong <chris@chrisarmstrong.me>
2014-01-23 19:49:05 -0800

    Adds platform versions to set_attributes_from_version_spec

--- spec/set_attributes_from_version_spec.rb
+++ spec/set_attributes_from_version_spec.rb
@@ -2,47 +2,50 @@ require 'spec_helper'
 
 describe 'java::set_attributes_from_version' do
   platforms = {
-    'rhel' => {
+    'centos-6.4' => {
       'java_home' => '/usr/lib/jvm/java-1.6.0',
       'packages' => ['java-1.6.0-openjdk', 'java-1.6.0-openjdk-devel']
     },
-    'fedora' => {
+    'redhat-6.3' => {
       'java_home' => '/usr/lib/jvm/java-1.6.0',
       'packages' => ['java-1.6.0-openjdk', 'java-1.6.0-openjdk-devel']
     },
-    'freebsd' => {
+    'freebsd-9.1' => {
       'java_home' => "/usr/local/openjdk6",
       'packages' => ["openjdk6"]
     },
-    'arch' => {
-      'java_home' => "/usr/lib/jvm/java-6-openjdk",
-      'packages' => ["openjdk6"]
+    'debian-7.0' => {
+      'java_home' => '/usr/lib/jvm/java-6-openjdk-amd64',
+      'packages' => ['openjdk-6-jdk', 'openjdk-6-jre-headless']
     },
-    'debian' => {
+    'ubuntu-12.04' => {
+      'java_home' => '/usr/lib/jvm/java-6-openjdk-amd64',
+      'packages' => ['openjdk-6-jdk', 'openjdk-6-jre-headless']
+    },
+    'debian-6.0.5' => {
       'java_home' => '/usr/lib/jvm/java-6-openjdk',
       'packages' => ['openjdk-6-jdk', 'openjdk-6-jre-headless']
     },
-    'smartos' => {
+    'ubuntu-10.04' => {
+      'java_home' => '/usr/lib/jvm/java-6-openjdk',
+      'packages' => ['openjdk-6-jdk', 'openjdk-6-jre-headless']
+    },
+    'smartos-joyent_20130111T180733Z' => {
       'java_home' => '/opt/local/java/sun6',
       'packages' => ['sun-jdk6', 'sun-jre6']
     },
-    'windows' => {
+    'windows-2008R2' => {
       'java_home' => nil,
       'packages' => []
-    },
-    'foo' => {
-      'java_home' => "/usr/lib/jvm/default-java",
-      'packages' => ["openjdk-6-jdk"]
     }
   }
 
   platforms.each do |platform, params|
-    context "#{platform}" do
-      let(:chef_run) do
-        runner = ChefSpec::Runner.new
-        runner.node.set['platform_family'] = platform
-        runner.converge(described_recipe)
-      end
+    parts = platform.split('-')
+    os = parts[0]
+    version = parts[1]
+    context "On #{os} #{version}" do
+      let(:chef_run) { ChefSpec::Runner.new(:version => version, :platform => os).converge(described_recipe) }
 
       it 'has the correct java_home' do
         expect(chef_run.node['java']['java_home']).to eq(params['java_home'])
