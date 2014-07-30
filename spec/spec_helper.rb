044a338b14803beb353484cd0e0c467cea0e723c
Eric Helgeson <erichelgeson@gmail.com>
2014-07-30 11:30:05 -0500

    rspec depricated config.treat_symbols_as_metadata_keys_with_true_values

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ RSpec.configure do |config|
   config.color = true
   config.tty = true
   config.formatter = :documentation
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
   config.expect_with :rspec do |c|
