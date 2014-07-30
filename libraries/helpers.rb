dd4ba1309f1a83d79b37caa89c37a3c056f488f4
Jean-Francois Theroux <me@failshell.io>
2014-07-29 22:15:45 -0400

    using new attribute name

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -37,19 +37,19 @@ module Opscode
     def alternatives_priority
       if @jdk_version == '6'
         # 'accepted' default for java 6
-        @node['java']['priority'] || 1061
+        @node['java']['alternatives_priority'] || 1061
       elsif @jdk_version == '7'
         # i just made this number up
-        @node['java']['priority'] || 1100
+        @node['java']['alternatives_priority'] || 1100
       elsif @jdk_version.to_i > 7
         # just a guard against the incoming java 8
         # so this cookbook will actually work for.. new versions of java
-        @node['java']['priority'] || 1110
+        @node['java']['alternatives_priority'] || 1110
       else
         # it's not 6, it's not 7, it's not newer than
         # 7, but we probably want to install it, so
         # override 6's priority. arbitrary number.
-        @node['java']['priority'] || 1062
+        @node['java']['alternatives_priority'] || 1062
       end
     end
 
