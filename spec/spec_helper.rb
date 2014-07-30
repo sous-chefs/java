1cb4a5f625da6bbc04efd75f777e91866833308e
Eric Helgeson <erichelgeson@gmail.com>
2014-07-30 09:32:17 -0500

    New synax for color in rspec3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
-  config.color_enabled = true
+  config.color = true
   config.tty = true
   config.formatter = :documentation
   config.treat_symbols_as_metadata_keys_with_true_values = true
