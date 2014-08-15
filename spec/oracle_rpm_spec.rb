611e3c131eb1b257c8fc25a74d2551a96d638570
Sasha Gerrand <sasha.gerrand@fundingcircle.com>
2014-08-15 10:00:06 +0100

    Ratified tests, improved coverage

--- spec/oracle_rpm_spec.rb
+++ spec/oracle_rpm_spec.rb
@@ -11,6 +11,94 @@ describe 'java::oracle_rpm' do
     expect(chef_run).to include_recipe('java::set_java_home')
   end
 
+  describe 'update-java-alternatives' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: platform, version: version) do |node|
+        node.automatic['java']['install_flavor'] = 'oracle_rpm'
+        node.set['java']['set_default'] = true
+      end.converge(described_recipe)
+    end
+
+    describe 'for RHEL' do
+      let(:platform) { 'redhat' }
+      let(:version) { '6.5' }
+
+      it 'does not run bash command' do
+        expect(chef_run).not_to run_bash('update-java-alternatives')
+      end
+    end
+  end
+
+  describe 'package_name attribute' do
+    describe 'using default value' do
+      let(:chef_run) do
+        ChefSpec::Runner.new do |node|
+          node.automatic['java']['install_flavor'] = 'oracle_rpm'
+        end.converge(described_recipe)
+      end
+
+      it 'does not install package_name' do
+        expect(chef_run).not_to install_package('')
+      end
+    end
+
+    context 'when package_name is set' do
+      let(:chef_run) do
+        ChefSpec::Runner.new do |node|
+          node.automatic['java']['install_flavor'] = 'oracle_rpm'
+          node.set['java']['oracle_rpm']['package_name'] = 'prime-caffeine'
+        end.converge(described_recipe)
+      end
+
+      it 'installs package_name' do
+        expect(chef_run).to install_package('prime-caffeine')
+      end
+    end
+
+    context 'when package_name and package_version is set' do
+      let(:chef_run) do
+        ChefSpec::Runner.new do |node|
+          node.automatic['java']['install_flavor'] = 'oracle_rpm'
+          node.set['java']['oracle_rpm']['package_name'] = 'prime-caffeine'
+          node.set['java']['oracle_rpm']['package_version'] = '8.7.6-goldmaster'
+        end.converge(described_recipe)
+      end
+
+      it 'installs package_name with specific version' do
+        expect(chef_run).to install_package('prime-caffeine').with(
+          version: '8.7.6-goldmaster'
+        )
+      end
+    end
+
+    context 'when type is set' do
+      let(:chef_run) do
+        ChefSpec::Runner.new do |node|
+          node.automatic['java']['install_flavor'] = 'oracle_rpm'
+          node.set['java']['oracle_rpm']['type'] = 'jdk'
+        end.converge(described_recipe)
+      end
+
+      it 'installs type' do
+        expect(chef_run).to install_package('jdk')
+      end
+    end
+
+    context 'when package_name and type are set' do
+      let(:chef_run) do
+        ChefSpec::Runner.new do |node|
+          node.automatic['java']['install_flavor'] = 'oracle_rpm'
+          node.set['java']['oracle_rpm']['package_name'] = 'top-shelf-beans'
+          node.set['java']['oracle_rpm']['type'] = 'jdk'
+        end.converge(described_recipe)
+      end
+
+      it 'installs package_name instead of type' do
+        expect(chef_run).to install_package('top-shelf-beans')
+      end
+    end
+  end
+
   describe 'type attribute' do
     let(:chef_run) do
       ChefSpec::Runner.new do |node|
@@ -60,78 +148,6 @@ describe 'java::oracle_rpm' do
 
       it 'raises an error' do
         expect { chef_run }.to raise_error
-      end
-    end
-  end
-
-  describe 'update-java-alternatives' do
-    let(:chef_run) do
-      ChefSpec::Runner.new(platform: platform, version: version) do |node|
-        node.automatic['java']['install_flavor'] = 'oracle_rpm'
-        node.set['java']['set_default'] = true
-      end.converge(described_recipe)
-    end
-
-    describe 'for RHEL' do
-      let(:platform) { 'redhat' }
-      let(:version) { '6.5' }
-
-      it 'does not run bash command' do
-        expect(chef_run).not_to run_bash('update-java-alternatives')
-      end
-    end
-  end
-
-  describe 'package_name' do
-    context 'for default values' do
-      let(:chef_run) do
-        ChefSpec::Runner.new do |node|
-          node.automatic['java']['install_flavor'] = 'oracle_rpm'
-        end.converge(described_recipe)
-      end
-
-      it 'does not install package_name' do
-        expect(chef_run).not_to install_package('')
-      end
-    end
-
-    context 'when package_name is set' do
-      let(:chef_run) do
-        ChefSpec::Runner.new do |node|
-          node.automatic['java']['install_flavor'] = 'oracle_rpm'
-          node.set['java']['oracle_rpm']['package_name'] = 'prime-caffeine'
-        end.converge(described_recipe)
-      end
-
-      it 'installs package_name if set' do
-        expect(chef_run).to install_package('prime-caffeine')
-      end
-    end
-
-    context 'when type is set' do
-      let(:chef_run) do
-        ChefSpec::Runner.new do |node|
-          node.automatic['java']['install_flavor'] = 'oracle_rpm'
-          node.set['java']['oracle_rpm']['type'] = 'jdk'
-        end.converge(described_recipe)
-      end
-
-      it 'installs type if set' do
-        expect(chef_run).to install_package('jdk')
-      end
-    end
-
-    context 'when package_name and type are set' do
-      let(:chef_run) do
-        ChefSpec::Runner.new do |node|
-          node.automatic['java']['install_flavor'] = 'oracle_rpm'
-          node.set['java']['oracle_rpm']['package_name'] = 'top-shelf-beans'
-          node.set['java']['oracle_rpm']['type'] = 'jdk'
-        end.converge(described_recipe)
-      end
-
-      it 'installs package_name instead of type' do
-        expect(chef_run).to install_package('top-shelf-beans')
       end
     end
   end
