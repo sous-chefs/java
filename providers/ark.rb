0c5c0418541b3d8bfd9a0d54de0f3771a0d29964
Jay Zhu <tingjie.zhu@autodesk.com>
2014-08-28 17:48:16 -0700

    In the ark provider, it doesn't compare the MD5 sum with the right value which causes Java cookbook always download tarball from oracle server

--- providers/ark.rb
+++ providers/ark.rb
@@ -53,8 +53,8 @@ def oracle_downloaded?(download_path, new_resource)
   if ::File.exists? download_path
     require 'digest'
     if new_resource.checksum =~ /^[0-9a-f]{32}$/
-      downloaded_sha =  Digest::MD5.file(download_path).hexdigest
-      downloaded_sha == new_resource.md5 
+      downloaded_md5 =  Digest::MD5.file(download_path).hexdigest
+      downloaded_md5 == new_resource.checksum
     else
       downloaded_sha =  Digest::SHA256.file(download_path).hexdigest
       downloaded_sha == new_resource.checksum
