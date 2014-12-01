260a7d8777948e93db7c5190ffc698ac31098e4d
tenbada <tenbada@gmail.com>
2014-12-02 01:45:59 +0900

    call iLoveYou()

--- src/git_fork_Java/MyClass.java
+++ src/git_fork_Java/MyClass.java
@@ -3,6 +3,7 @@ package git_fork_Java;
 public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("print");
+		new MyClass().iLoveYou();
 	}
 	
 	public void iLoveYou() {
