d4e59f5b09ca7a350f98f30ea03e77a299ca0503
William Herbert Lichtenberger <william.lichtenberger@perceptivesofware.com>
2015-02-10 10:10:19 -0600

    Update metadata.rb to contain source and issue information for supermarket and chef-repo convenience

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name              "java"
 maintainer        "Agile Orbit"
 maintainer_email  "info@agileorbit.com"
+source_url        "https://github.com/agileorbit-cookbooks/java"
+issues_url 	      "https://github.com/agileorbit-cookbooks/java/issues"
 license           "Apache 2.0"
 description       "Installs Java runtime."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
