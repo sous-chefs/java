7a2d60a4e61430710d17b475e9c2072dc678c0d1
Robert Northard <robertnorthard@googlemail.com>
2015-09-11 00:02:36 +0100

    Chef < 12 compatibility

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description       "Installs Java runtime."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.32.1"
 
-source_url 'https://github.com/agileorbit-cookbooks/java'
-issues_url 'https://github.com/agileorbit-cookbooks/java/issues'
+source_url 'https://github.com/agileorbit-cookbooks/java' if respond_to?(:source_url)
+issues_url 'https://github.com/agileorbit-cookbooks/java/issues' if respond_to?(:issues_url)
 
 recipe "java::default", "Installs Java runtime"
 recipe "java::default_java_symlink", "Updates /usr/lib/jvm/default-java"
