e60a3a9a8de715815610c7a7b9287e34e2913630
Robert Northard <robertnorthard@googlemail.com>
2015-09-10 23:46:31 +0100

    Added source and issues url to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description       "Installs Java runtime."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.32.1"
 
+source_url 'https://github.com/agileorbit-cookbooks/java'
+issues_url 'https://github.com/agileorbit-cookbooks/java/issues'
+
 recipe "java::default", "Installs Java runtime"
 recipe "java::default_java_symlink", "Updates /usr/lib/jvm/default-java"
 recipe "java::ibm", "Installs the JDK for IBM"
