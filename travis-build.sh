b5bc5ad2ee638bd6a128d8d825cb9b4a189c39b0
Eric Helgeson <erichelgeson@gmail.com>
2016-07-15 10:55:09 -0500

    only deploy if in travis and on master branch

--- travis-build.sh
+++ travis-build.sh
@@ -5,6 +5,7 @@ echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
 echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_TAG: $TRAVIS_TAG"
 echo "TRAVIS_PWD: $PWD"
+echo "TRAVIS: $TRAVIS"
 
 /opt/chefdk/embedded/bin/chef --version || exit 1
 /opt/chefdk/embedded/bin/rubocop --version || exit 1
@@ -16,13 +17,11 @@ echo "TRAVIS_PWD: $PWD"
 if [[ -n $TRAVIS_TAG && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   echo "Deploying java cookbook - release"
   /opt/chefdk/embedded/bin/knife cookbook site share java "Other" -o ../ --config .travis/config.rb
-else
+elif [[ $TRAVIS && $TRAVIS_BRANCH = 'master' ]]; then
   echo "Deploying java-snapshot cookbook"
-  SED=sed
-  if [ "$(uname)" = "Darwin" ]; then
-    SED=gsed
-  fi
-  $SED -i '1 s/^.*$/name \"java-snapshot\"/g' metadata.rb
+  sed -i '1 s/^.*$/name \"java-snapshot\"/g' metadata.rb
   /opt/chefdk/embedded/bin/knife cookbook site share java-snapshot "Other" -o ../ --config .travis/config.rb
   git checkout metadata.rb
+else
+  echo "Not in Travis, skipping deploy."
 fi
