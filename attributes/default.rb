2d1d57f56d2a6a40bd4c941729908cf4f3fa6ca6
Marc Arndt <marc@marcarndt.com>
2016-12-27 10:03:47 +0200

    fixing food critic issue where there is a space missing after the #

--- attributes/default.rb
+++ attributes/default.rb
@@ -50,7 +50,7 @@ else
   default['java']['install_flavor'] = 'openjdk'
 end
 
-#S390(X) - IBM zSeries Architecture - only IBM jre / jdk can be used. Download from https://developer.ibm.com/javasdk/downloads/
+# S390(X) - IBM zSeries Architecture - only IBM jre / jdk can be used. Download from https://developer.ibm.com/javasdk/downloads/
 if node['kernel']['machine'].start_with?('s390')
   default['java']['install_flavor'] = 'ibm'
 end
