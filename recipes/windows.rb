bded79ae93a2b573f58967fc2982df28fabdc4e3
Tim Smith <tsmith@chef.io>
2017-02-16 15:42:51 -0800

    aws::default does nothing now. Remove it
    We auto install the sdk now when you use the provider
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/windows.rb
+++ recipes/windows.rb
@@ -36,8 +36,6 @@ uri = ::URI.parse(node['java']['windows']['url'])
 cache_file_path = File.join(Chef::Config[:file_cache_path], File.basename(::URI.unescape(uri.path)))
 
 if s3_bucket && s3_remote_path
-  include_recipe 'aws::default' # install right_aws gem for aws_s3_file
-
   aws_s3_file cache_file_path do
     aws_access_key_id aws_access_key_id
     aws_secret_access_key aws_secret_access_key
