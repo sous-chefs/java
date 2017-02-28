42bb084f8921deb09f4cc5812fc58085e9e39383
Tim Smith <tsmith@chef.io>
2017-02-27 16:17:41 -0800

    Require Chef 12+
    The deps already require Chef 12 so this cookbook already requires 12 without Berkshelf pinning fun.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -48,5 +48,6 @@ depends 'apt'
 depends 'windows'
 depends 'homebrew'
 
-source_url 'https://github.com/agileorbit-cookbooks/java' if respond_to?(:source_url)
-issues_url 'https://github.com/agileorbit-cookbooks/java/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/agileorbit-cookbooks/java'
+issues_url 'https://github.com/agileorbit-cookbooks/java/issues'
+chef_version '>= 12.1'
