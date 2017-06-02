4b470fb6af5dc1f5b51d18575d963f32f3b522c6
Tim Smith <tsmith@chef.io>
2017-06-02 11:54:41 -0700

    Remove Oracle 6/7 tests that won't run
    Oracle removed these JDKs from their site
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/test_java/recipes/oracle_direct.rb
+++ test/fixtures/cookbooks/test_java/recipes/oracle_direct.rb
@@ -1,4 +1,5 @@
+node.default['java']['jdk_version'] = '8'
 node.default['java']['oracle']['accept_oracle_download_terms'] = true
 
 include_recipe 'test_java::base'
-include_recipe 'java::oracle_direct'
+include_recipe 'java::oracle'
--- test/fixtures/cookbooks/test_java/recipes/oracle_rpm7.rb
+++
@@ -1,6 +0,0 @@
-node.default['java']['jdk_version'] = '7'
-node.default['java']['install_flavor'] = 'oracle_rpm'
-node.default['java']['oracle']['accept_oracle_download_terms'] = true
-
-include_recipe 'test_java::base'
-include_recipe 'java::default'
