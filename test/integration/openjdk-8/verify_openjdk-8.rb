023b8cedb616cd7b338df915776c3932feb2866e
Tim Smith <tsmith@chef.io>
2017-06-02 21:52:29 -0700

    Convert integration tests to Inspec
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ test/integration/openjdk-8/verify_openjdk-8.rb
@@ -0,0 +1,19 @@
+# the right version of java is installed
+describe command('java -version') do
+  its('stdout') { should include '1.8.0' }
+end
+
+# env is properly setup
+describe os_env('JAVA_HOME') do
+  its('content') { should include 'java-1.8.0' }
+end
+
+# alternatives were properly set
+describe command('alternatives --display jar') do
+  its('stdout') { should include 'java-1.8.0' }
+end
+
+# the cert was installed into the keystore
+describe command('$JAVA_HOME/bin/keytool -list -storepass changeit -keystore $JAVA_HOME/jre/lib/security/cacerts -alias java_certificate_test') do
+  its('stdout') { should include '9D:9E:EA:E6:5F:D2:C8:34:93:6E:5C:65:EE:00:46:A9:CD:E4:F1:83' }
+end
