65b781de3bd4ab384f421fb8cb88c590aeeb7924
Tim Smith <tsmith@chef.io>
2017-06-03 13:33:00 -0700

    Avoid apt-update on opensuse
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/test_java/recipes/base.rb
+++ test/fixtures/cookbooks/test_java/recipes/base.rb
@@ -1,7 +1,7 @@
 node.default['java']['ark_retries'] = 2
 node.default['java']['ark_retry_delay'] = 10
 
-apt_update
+apt_update if platform_family?('debian')
 
 # we need bash for bats on FreeBSD
 package 'bash' if platform_family?('freebsd')
