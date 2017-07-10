491b2eb967350f4cecee9c562aa4e5b63e6cb087
Ed Flanagan <ed@index.com>
2017-07-10 13:35:08 -0700

    Handle 'default' homebrew version
    * Java 8 is the 'default' Caskroom cask, i.e. there
      is no 'java8' cask, it's 'java'
    * Add warning that java7 was removed from caskroom/versions

--- recipes/homebrew.rb
+++ recipes/homebrew.rb
@@ -3,6 +3,21 @@ include_recipe 'homebrew::cask'
 include_recipe 'java::notify'
 
 homebrew_tap 'caskroom/versions'
-homebrew_cask "java#{node['java']['jdk_version']}" do
+
+cask_default_java_version = '8'
+if node['java']['jdk_version'].to_s == cask_default_java_version
+  pkg_name = 'java'
+else
+  pkg_name = "java#{node['java']['jdk_version']}"
+end
+
+if node['java']['jdk_version'].to_s == '7'
+  log 'java-7-cask-removed' do
+    message 'java7 has been removed from caskroom/versions. See https://github.com/caskroom/homebrew-versions/pull/3914'
+    level :warn
+  end
+end
+
+homebrew_cask pkg_name do
   notifies :write, 'log[jdk-version-changed]', :immediately
 end
