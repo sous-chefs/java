082d27e93cd4a8777442bba1f0bc1dbf4ce9cb12
Stephane Jeandeaux <stephane.jeandeaux@gmail.com>
2018-07-10 16:36:54 -0400

    [test] try to test

--- test/fixtures/cookbooks/test/recipes/openjdk7.rb
+++ test/fixtures/cookbooks/test/recipes/openjdk7.rb
@@ -1,5 +1,5 @@
 node.default['java']['jdk_version'] = '7'
 
-include_recipe 'test_java::base'
+include_recipe 'test::base'
 include_recipe 'java::default'
-include_recipe 'test_java::java_cert'
+include_recipe 'test::java_cert'
