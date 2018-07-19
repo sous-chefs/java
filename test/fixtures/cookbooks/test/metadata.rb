e607d0b58c4a45da0ebcb8a18c7a5cbe9e4cdb56
Tim Smith <tsmith@chef.io>
2018-07-19 08:20:02 -0700

    Fix test cookbook name
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/test/metadata.rb
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -1,5 +1,5 @@
-name             'test_java'
-maintainer       'test_java cookbook'
+name             'test'
+maintainer       'test cookbook'
 license          'Apache-2.0'
 description      'A test cookbook for the java cookbook'
 version          '0.1.0'
