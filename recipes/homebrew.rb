8d1a0145042581ebdea8543aa3713d3bc27c125a
Mihai Costea <mihai.costea@telenav.com>
2019-10-09 09:43:00 +0300

    Switch homebrew tap to homebrew/cask-versions

--- recipes/homebrew.rb
+++ recipes/homebrew.rb
@@ -2,7 +2,7 @@ include_recipe 'homebrew'
 include_recipe 'homebrew::cask'
 include_recipe 'java::notify'
 
-homebrew_tap 'caskroom/versions'
+homebrew_tap 'homebrew/cask-versions'
 homebrew_cask "#{node['java']['homebrew']['cask']}#{node['java']['jdk_version']}" do
   notifies :write, 'log[jdk-version-changed]', :immediately
 end
