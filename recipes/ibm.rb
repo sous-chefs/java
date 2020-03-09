d1d57c35f6e4ddcee8686e60549bd7994c8753c3
Dan Webb <dan.webb@damacus.io>
2020-03-09 22:55:19 +0000

    Update recipes/ibm.rb
    Co-Authored-By: John Roesler <johnrroesler@gmail.com>

--- recipes/ibm.rb
+++ recipes/ibm.rb
@@ -1,8 +1,8 @@
 
 Chef::Log.fatal("
 
-IBM recipe is now deprecated
-Using one of the documented install resources
+IBM recipe is now removed
+Use one of the documented install resources
 See the documentation folder for a list of resources
 
 ")
