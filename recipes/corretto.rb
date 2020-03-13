7d1f3afcf2fa3c1fe9ffb4df33eac48b6e9af3fa
Dan Webb <dan.webb@damacus.io>
2020-03-13 18:28:02 +0000

    Update recipes/corretto.rb
    Co-Authored-By: Tim Smith <tsmith@chef.io>

--- recipes/corretto.rb
+++ recipes/corretto.rb
@@ -1,7 +1,7 @@
 
 Chef::Log.fatal("
 
-Corretto recipe is now deprecated
+java::corretto recipe is now deprecated
 Using the corrett_install resource is now recommended
 See: documentation/resources/install_corretto.md for help
 
