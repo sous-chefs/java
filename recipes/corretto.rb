8c3008ee446b0ed90164784509e8017a3a2f5ffe
Dan Webb <dan.webb@damacus.io>
2020-03-23 15:31:19 +0000

    Fix links to documentation

--- recipes/corretto.rb
+++ recipes/corretto.rb
@@ -3,7 +3,7 @@ Chef::Log.fatal("
 
 java::corretto recipe is now deprecated
 Using the corrett_install resource is now recommended
-See: documentation/resources/install_corretto.md for help
+See: https://github.com/sous-chefs/java/blob/master/documentation/resources/corretto.md for help
 
 ")
 
