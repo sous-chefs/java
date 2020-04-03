8690adbae82827a1307408b592faf7ee527c133b
Dan Webb <dan.webb@damacus.io>
2020-04-03 11:06:25 +0100

    Add MacOS support for AdoptOpenJDK

--- resources/adoptopenjdk_install.rb
+++ resources/adoptopenjdk_install.rb
@@ -27,70 +27,105 @@ property :alternatives_priority, Integer, default: 1,
 property :reset_alternatives, [true, false], default: true,
                                              description: 'Whether to reset alternatives before setting'
 
+# Homebrew options
+property :tap_full, [true, false], default: true, description: 'Perform a full clone on the tap, as opposed to a shallow clon.'
+property :tap_url, String, description: 'The URL of the tap'
+property :cask_options, String, description: 'Options to pass to the brew command during installation'
+property :homebrew_path, String, description: 'The path to the homebrew binary'
+property :owner, [String, Integer], description: 'The owner of the Homebrew installation'
+
 action :install do
-  extract_dir = new_resource.java_home.split('/')[0..-2].join('/')
-  parent_dir = new_resource.java_home.split('/')[0..-3].join('/')
-  tarball_name = new_resource.url.split('/').last
+  case node['platform_family']
+  when 'macos'
+    adoptopenjdk_macos_install 'homebrew' do
+      tap_full new_resource.tap_full
+      tap_url new_resource.tap_url
+      cask_options new_resource.cask_options
+      homebrew_path new_resource.homebrew_path
+      owner new_resource.owner
+      action :remove
+    end
+  when 'windows'
+    log 'not yet implemented'
+  else
+    extract_dir = new_resource.java_home.split('/')[0..-2].join('/')
+    parent_dir = new_resource.java_home.split('/')[0..-3].join('/')
+    tarball_name = new_resource.url.split('/').last
 
-  directory parent_dir do
-    owner new_resource.java_home_owner
-    group new_resource.java_home_group
-    mode new_resource.java_home_mode
-    recursive true
-  end
+    directory parent_dir do
+      owner new_resource.java_home_owner
+      group new_resource.java_home_group
+      mode new_resource.java_home_mode
+      recursive true
+    end
 
-  remote_file "#{Chef::Config[:file_cache_path]}/#{tarball_name}" do
-    source new_resource.url
-    checksum new_resource.checksum
-    retries new_resource.retries
-    retry_delay new_resource.retry_delay
-    mode '644'
-  end
+    remote_file "#{Chef::Config[:file_cache_path]}/#{tarball_name}" do
+      source new_resource.url
+      checksum new_resource.checksum
+      retries new_resource.retries
+      retry_delay new_resource.retry_delay
+      mode '644'
+    end
 
-  archive_file "#{Chef::Config[:file_cache_path]}/#{tarball_name}" do
-    destination extract_dir
-  end
+    archive_file "#{Chef::Config[:file_cache_path]}/#{tarball_name}" do
+      destination extract_dir
+    end
 
-  node.default['java']['java_home'] = new_resource.java_home
+    node.default['java']['java_home'] = new_resource.java_home
 
-  template "/usr/lib/jvm/.java-#{new_resource.version}-adoptopenjdk-#{new_resource.variant}.jinfo" do
-    cookbook 'java'
-    source 'jinfo.erb'
-    owner new_resource.java_home_owner
-    group new_resource.java_home_group
-    variables(
-      priority: new_resource.alternatives_priority,
-      bin_cmds: new_resource.bin_cmds,
-      name: extract_dir.split('/').last,
-      app_dir: new_resource.java_home
-    )
-    only_if { platform_family?('debian') }
-  end
+    template "/usr/lib/jvm/.java-#{new_resource.version}-adoptopenjdk-#{new_resource.variant}.jinfo" do
+      cookbook 'java'
+      source 'jinfo.erb'
+      owner new_resource.java_home_owner
+      group new_resource.java_home_group
+      variables(
+        priority: new_resource.alternatives_priority,
+        bin_cmds: new_resource.bin_cmds,
+        name: extract_dir.split('/').last,
+        app_dir: new_resource.java_home
+      )
+      only_if { platform_family?('debian') }
+    end
 
-  java_alternatives 'set-java-alternatives' do
-    java_location new_resource.java_home
-    bin_cmds new_resource.bin_cmds
-    priority new_resource.alternatives_priority
-    default new_resource.default
-    reset_alternatives new_resource.reset_alternatives
-    action :set
+    java_alternatives 'set-java-alternatives' do
+      java_location new_resource.java_home
+      bin_cmds new_resource.bin_cmds
+      priority new_resource.alternatives_priority
+      default new_resource.default
+      reset_alternatives new_resource.reset_alternatives
+      action :set
+    end
   end
 end
 
 action :remove do
-  extract_dir = new_resource.java_home.split('/')[0..-2].join('/')
+  case node['platform_family']
+  when 'macos'
+    adoptopenjdk_macos_install 'homebrew' do
+      tap_full new_resource.tap_full
+      tap_url new_resource.tap_url
+      cask_options new_resource.cask_options
+      homebrew_path new_resource.homebrew_path
+      owner new_resource.owner
+      action :remove
+    end
+  when 'windows'
+    log 'not yet implemented'
+  else
+    extract_dir = new_resource.java_home.split('/')[0..-2].join('/')
 
-  java_alternatives 'unset-java-alternatives' do
-    java_location new_resource.java_home
-    bin_cmds new_resource.bin_cmds
-    only_if { ::File.exist?(extract_dir) }
-    action :unset
-  end
+    java_alternatives 'unset-java-alternatives' do
+      java_location new_resource.java_home
+      bin_cmds new_resource.bin_cmds
+      only_if { ::File.exist?(extract_dir) }
+      action :unset
+    end
 
-  directory "AdoptOpenJDK removal of #{extract_dir}" do
-    path extract_dir
-    recursive true
-    only_if { ::File.exist?(extract_dir) }
-    action :delete
+    directory "AdoptOpenJDK removal of #{extract_dir}" do
+      path extract_dir
+      recursive true
+      only_if { ::File.exist?(extract_dir) }
+      action :delete
+    end
   end
 end
---
+++ resources/adoptopenjdk_macos_install.rb
@@ -0,0 +1,45 @@
+property :tap_full, [true, false], default: true, description: 'Perform a full clone on the tap, as opposed to a shallow clon.'
+property :tap_url, String, description: 'The URL of the tap'
+property :cask_options, String, description: 'Options to pass to the brew command during installation'
+property :homebrew_path, String, description: 'The path to the homebrew binary'
+property :owner, [String, Integer], description: 'The owner of the Homebrew installation'
+property :version, String, default: 'adoptopenjdk14', equal_to: %w(adoptopenjdk8 adoptopenjdk8-openj9 adoptopenjdk8-openj9-large
+                                                                   adoptopenjdk9
+                                                                   adoptopenjdk10
+                                                                   adoptopenjdk11 adoptopenjdk11-openj9 adoptopenjdk11-openj9-large
+                                                                   adoptopenjdk12 adoptopenjdk12-openj9 adoptopenjdk12-openj9-large
+                                                                   adoptopenjdk13 adoptopenjdk13-openj9 adoptopenjdk13-openj9-large
+                                                                   adoptopenjdk14 adoptopenjdk14-openj9 adoptopenjdk14-openj9-large)
+
+action :install do
+  homebrew_tap 'AdoptOpenJDK/openjdk' do
+    full               new_resource.tap_full
+    homebrew_path      new_resource.tap_homebrew_path
+    owner              new_resource.owner
+    url                new_resource.tap_url
+    action             :tap
+  end
+
+  homebrew_cask "adoptopenjdk#{new_resource.version}" do
+    homebrew_path      new_resource.homebrew_path
+    install_cask       true
+    options            new_resource.cask_options
+    owner              new_resource.owner
+    action             :install
+  end
+end
+
+action :remove do
+  homebrew_tap 'AdoptOpenJDK/openjdk' do
+    homebrew_path      new_resource.tap_homebrew_path
+    owner              new_resource.owner
+    action :untap
+  end
+
+  homebrew_cask "adoptopenjdk#{new_resource.version}" do
+    homebrew_path      new_resource.cask_path
+    options            new_resource.cask_options
+    owner              new_resource.cask_owner
+    action             :remove
+  end
+end
