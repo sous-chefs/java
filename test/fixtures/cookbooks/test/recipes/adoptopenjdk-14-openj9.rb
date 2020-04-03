f4357060f893fee9df37c3bf1ad2057fa119a24b
Dan Webb <dan.webb@damacus.io>
2020-04-03 11:38:52 +0100

    Revert CI change

---
+++ test/fixtures/cookbooks/test/recipes/adoptopenjdk-14-openj9.rb
@@ -0,0 +1,26 @@
+apt_update
+
+version = '13'
+variant = 'openj9'
+
+adoptopenjdk_install version do
+  variant variant
+end
+
+cookbook_file '/tmp/java_certificate_test.pem' do
+  source 'java_certificate_test.pem'
+end
+
+java_certificate 'java_certificate_test' do
+  cert_file '/tmp/java_certificate_test.pem'
+  java_version version
+end
+
+java_certificate 'java_certificate_ssl_endpoint' do
+  ssl_endpoint 'google.com:443'
+  java_version version
+end
+
+cookbook_file '/tmp/UnlimitedSupportJCETest.jar' do
+  source 'UnlimitedSupportJCETest.jar'
+end
