a00bb555803ee3a0a218639ef092fb8abacca345
Dan Webb <dan.webb@damacus.io>
2020-04-16 11:02:07 +0100

    SQUASHME

--- resources/adoptopenjdk_macos_install.rb
+++ resources/adoptopenjdk_macos_install.rb
@@ -45,10 +45,10 @@ action :install do
     line "export JAVA_HOME=#{new_resource.java_home}"
   end
 
-  CI_USER = 'runner'
+  user = 'runner'
 
   append_if_no_line 'User JAVA_HOME' do
-    path "/Users/#{CI_RUNNER}/.profile"
+    path "/Users/#{user}/.profile"
     line "export JAVA_HOME=#{new_resource.java_home}"
   end
 
