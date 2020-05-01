9e76a1e2dcc9fdf3e373d003c81901154c85412d
Tyler Mauthe <me@tylermauthe.com>
2020-05-01 02:47:55 -0700

    Fix typo on openjdk_install documentation (#613)

--- recipes/openjdk.rb
+++ recipes/openjdk.rb
@@ -2,7 +2,7 @@
 Chef::Log.fatal("
 
 java::openjdk recipe is now deprecated
-Using the adoptopenjdk_install resource is now recommended
+Using the openjdk_install resource is now recommended
 See: https://github.com/sous-chefs/java/blob/master/documentation/resources/openjdk_install.md for help
 
 ")
