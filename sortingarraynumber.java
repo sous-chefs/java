d2dfb0c580f81df22286f4f87c98d188d0159a38
HG9040626 <68577284+HG9040626@users.noreply.github.com>
2020-10-21 20:43:03 +0530

    sortingarraynumber
    Java Program to sort an array.

---
+++ sortingarraynumber.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class JavaExample 
+{
+    public static void main(String[] args) 
+    {
+    	int count, temp;
+    	
+    	//User inputs the array size
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number of elements you want in the array: ");
+        count = scan.nextInt();
+    
+        int num[] = new int[count];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < count; i++) 
+        {
+            num[i] = scan.nextInt();
+        }
+        scan.close();
+        for (int i = 0; i < count; i++) 
+        {
+            for (int j = i + 1; j < count; j++) { 
+                if (num[i] > num[j]) 
+                {
+                    temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+        System.out.print("Array Elements in Ascending Order: ");
+        for (int i = 0; i < count - 1; i++) 
+        {
+            System.out.print(num[i] + ", ");
+        }
+        System.out.print(num[count - 1]);
+    }
+}
