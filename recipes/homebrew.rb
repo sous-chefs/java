e718926a6bbc75164a212550b6599447d0e03df5
Dan Webb <dan.webb@damacus.io>
2021-09-02 10:41:59 +0100

    Remove recipes (#660)
    * Remove recipes as they are confusing users
    
    Not all of our users are reading the errors in the logs, and are
    expecting the recipes to do something.
    The recipes have been warning since 8.0.0 (2020-03-30)
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- recipes/homebrew.rb
+++
@@ -1,9 +0,0 @@
-Chef::Log.fatal("
-
-The java::homebrew recipe is now deprecated
-Use the homebrew cookbook and resources in your wrapper cookbook
-See the documentation folder for a list of resources
-
-")
-
-raise 'Recipe used instead of custom resource'
