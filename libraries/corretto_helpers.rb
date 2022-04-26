8437399621e26717afef88947a574625a62a7b9a
Dan Webb <dan.webb@damacus.io>
2022-04-26 13:15:31 +0100

    Update Corretto URLs (#675)
    - Remove Correto 15 and 16
    - Add Corretto 17 and 18
    - Change the default download URL for Corretto to the versioned resources URL, rather than the latest.
    - Add default_corretto_minor helper to find out a default minor version
    for a given major
    - Fix default_corretto_minor helper to accept full versions
    - Move logic for full_version out of the major-to-minor method
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- libraries/corretto_helpers.rb
+++ libraries/corretto_helpers.rb
@@ -5,44 +5,43 @@ module Java
         node['kernel']['machine'].match?('aarch64') ? 'aarch64' : 'x64'
       end
 
-      def default_corretto_url(version)
-        "https://corretto.aws/downloads/latest/amazon-corretto-#{version}-#{corretto_arch}-linux-jdk.tar.gz"
-      end
-
       def default_corretto_bin_cmds(version)
         case version.to_s
         when '8'
           %w(appletviewer clhsdb extcheck hsdb idlj jar jarsigner java java-rmi.cgi javac javadoc javafxpackager javah javap javapackager jcmd jconsole jdb jdeps jfr jhat jinfo jjs jmap jps jrunscript jsadebugd jstack jstat jstatd keytool native2ascii orbd pack200 policytool rmic rmid rmiregistry schemagen serialver servertool tnameserv unpack200 wsgen wsimport xjc)
         when '11'
           %w(jaotc jar jarsigner java javac javadoc javap jcmd jconsole jdb jdeprscan jdeps jfr jhsdb jimage jinfo jjs jlink jmap jmod jps jrunscript jshell jstack jstat jstatd keytool pack200 rmic rmid rmiregistry serialver unpack200)
-        when '15'
-          %w(jaotc jar jarsigner java javac javadoc javap jcmd jconsole jdb jdeprscan jdeps jfr jhsdb jimage jinfo jlink jmap jmod jpackage jps jrunscript jshell jstack jstat jstatd keytool rmid rmiregistry serialver)
-        when '16'
+        when '15', '17', '18'
           %w(jaotc jar jarsigner java javac javadoc javap jcmd jconsole jdb jdeprscan jdeps jfr jhsdb jimage jinfo jlink jmap jmod jpackage jps jrunscript jshell jstack jstat jstatd keytool rmid rmiregistry serialver)
         else
           raise 'Corretto version not recognised'
         end
       end
 
-      def corretto_sub_dir(version, full_version = nil)
-        if full_version.nil?
-          case version
-          when '8'
-            ver = '8.302.08.1'
-          when '11'
-            ver = '11.0.12.7.1'
-          when '15'
-            ver = '15.0.2.7.1'
-          when '16'
-            ver = '16.0.2.7.1'
-          else
-            raise 'Corretto version not recognised'
-          end
+      def default_corretto_minor(version)
+        case version
+        when '8'
+          '8.332.08.1'
+        when '11'
+          '11.0.15.9.1'
+        when '17'
+          '17.0.3.6.1'
+        when '18'
+          '18.0.1.10.1'
         else
-          ver = full_version
+          raise 'Corretto version not recognised'
         end
+      end
 
+      def corretto_sub_dir(version, full_version = nil)
+        ver = full_version.nil? ? default_corretto_minor(version) : full_version
         "amazon-corretto-#{ver}-linux-#{corretto_arch}"
+      end
+
+      def default_corretto_url(version)
+        ver = version.include?('.') ? version : default_corretto_minor(version)
+
+        "https://corretto.aws/downloads/resources/#{ver}/amazon-corretto-#{ver}-linux-#{corretto_arch}.tar.gz"
       end
     end
   end
