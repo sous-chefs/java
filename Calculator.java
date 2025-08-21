0b0289637306d7b5a0d23572d8548a30ceb9106c
Adi-Sin101 <adiba0tahsin@gmai.com>
2025-08-21 18:34:32 +0600

    Created Student.java class and Calculator.java class with necessary implementations

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+    
+    // Addition method
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    // Subtraction method
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    // Multiplication method
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    // Division method
+    public double divide(int a, int b) {
+        
+        return (double) a / b;
+    }
+}
---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    private String name;
+    private int id;
+    
+    // Constructor
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+    
+    // Getters and Setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    
+}
